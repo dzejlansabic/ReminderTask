a66b5fcf529de8e88cef86ac39cffa79970cdb47
Dzejlan Sabic <dzejlan@softweare.ba>
2026-01-12 13:11:02 +0100

    Add appDbContext and models, implement get and post in controller.

---
+++ DTOs/ReminderResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ReminderTask.DTOs
+{
+    public class ReminderResponse
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; } = null!;
+        public DateTime SendAt { get; set; }
+        public string Status { get; set; } = null!;
+    }
+}
--- Models/Reminder.cs
+++ Models/Reminder.cs
@@ -1,12 +1,16 @@
-﻿namespace ReminderTask.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReminderTask.Models
 {
     public class Reminder
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
         public string Message { get; set; } = null!;
         public DateTime SendAt { get; set; }
         public string? Email { get; set; }
         public ReminderStatus Status { get; set; } = ReminderStatus.Scheduled;
-        public DateTime? SentAt { get; set; }
+        public DateTime? SentAt { get; set; } = DateTime.UtcNow;
     }
 }
